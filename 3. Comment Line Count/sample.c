7172a31d39ee3819bf5195de062d4ea56fa4d7db
Saad <100187174+istiakAHAMMEDsaad@users.noreply.github.com>
2024-02-22 00:18:15 +0600

    Add files via upload

---
+++ 3. Comment Line Count/sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// this is a single line comment
+int main()
+{
+    int number = 10;
+    double value = 1.67;
+    printf("The number is: %d \n", number);
+    return 0;
+}
+/*
+this is a
+multi line comment
+*/
